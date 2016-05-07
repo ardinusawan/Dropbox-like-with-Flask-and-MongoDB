adf492ff483764de349771d5452dca62780f3fb1
ardi.nusawan13@gmail.com <ardi.nusawan13@gmail.com>
2016-05-08 02:29:51 +0700

    adding new atribute

--- Sarang_Sharing/populateDB.py
+++ Sarang_Sharing/populateDB.py
@@ -7,7 +7,7 @@ from pymongo.errors import DuplicateKeyError
 
 def main():
     # Connect to the DB
-    collection = MongoClient()["gridfs_server_test"]["users"]
+    collection = MongoClient()["gridfs_server"]["users"]
 
     # Ask for data to store
     user = raw_input("Enter your username: ")
@@ -15,7 +15,7 @@ def main():
     pass_hash = generate_password_hash(password, method='pbkdf2:sha256')
     # Insert the user in the DB
     try:
-        collection.insert({"_id": user, "password": pass_hash})
+        collection.insert({"_id": user, "password": pass_hash, "usage": 0, "limit": 3000000})
         print "User created."
     except DuplicateKeyError:
         print "User already present in DB."
