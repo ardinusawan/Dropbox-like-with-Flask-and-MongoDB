39aad103d823b7e3ffa5736dc4e0a7d1fb4077e6
ardi.nusawan13@gmail.com <ardi.nusawan13@gmail.com>
2016-05-08 02:30:09 +0700

    rename DB server

--- Sarang_Sharing/config.py
+++ Sarang_Sharing/config.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '12345678'
-DATA_DB_NAME = 'gridfs_server_test'
+DATA_DB_NAME = 'gridfs_server'
 
 DATABASE = MongoClient(["localhost:27017"])[DATA_DB_NAME]
 POSTS_COLLECTION = DATABASE.posts
